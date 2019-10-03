ce4b0c7bf2454c683f42d7bf964cf95d5b0e4056
Rybin-AV <superadmin@FishPlace.localdomian>
2019-10-03 17:38:06 +0600

    Local

--- modules/disk.sh
+++ modules/disk.sh
@@ -21,10 +21,8 @@ get_list_block_device()
 auto_prepare_disk()
 {
 
-	_dia_inform "Please, Wait..."
 	echo ";" | sfdisk $dev >> log.txt
 	mkfs.ext4 -F ${dev}1 >> log.txt
-	_dia_notify  "Disk Prepare!"
 
 }
 
