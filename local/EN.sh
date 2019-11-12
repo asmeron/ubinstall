71ede689e90af17ef0ca4c98f241eddee918012b
Rybin-AV <superadmin@FishPlace.localdomian>
2019-11-12 17:53:26 +0600

    Update install

---
+++ local/EN.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+LIBUI_DIA_MENU_TEXT="Use the UP and DOWN arrows to navigate menus.  Use TAB to switch between buttons and ENTER to select."
+
+HELLO="Welcome to install UBLinux"
+
+HBM="Base Menu"
+ABM="Please choose action"
+
+PDISK="Prepare Hard Disk"
+PCOM="Install Components"
+PCONF="Configurate System"
+PIB="Install Bootloader"
+PEX="Exit"
+
+HMM="Choose Mode"
+AMM="Please Select Mode"
+
+Auto="Automatic"
+Manual="Manual"
+
+HDM="Menu Device"
+ADM="Please choose device"
+
+P_other="Other"
+P_view="View /dev"
+
+MD="Enter the block device"
+
+HPM="Manual Mode"
+APM="Please choose action"
+
+PPD="Partitioning Disk"
+PFD="Format Partition"
+PCS="Create Swap File"
+PD="Done"
+
+SD="Enter the size swap file (MB)"
+
+HFM="Choose File System"
+AFM="Please choose File System"
+SP="Select Partition(s)"
+
+HCD="Choose Distrub"
+ACD="Please Select distrub"
+NI="Network Install"
+SPA="Select Package"
+
+HN="Enter the Host Name"
+DM="Enter the Domen"
+UN="Enter the User Name"
+UP="Enter the password for "
+
+PW="Please, Wait..."
+DP="Disk Prepare!"
+
+HSM="Swap Menu"
+ASM="Choose Size Swap File"
+CUSTOM="Custom Size"
